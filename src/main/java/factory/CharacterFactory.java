975e96fa91d45bc903f6a468b652ede59cc14736
vitaliyvorobyov <vorobyov@ucu.edu.ua>
2018-12-28 12:49:55 +0200

    Fixed CharacterFactory

--- src/main/java/factory/CharacterFactory.java
+++ src/main/java/factory/CharacterFactory.java
@@ -3,13 +3,13 @@ package factory;
 import character.*;
 import character.Character;
 import config.Config;
+import lombok.SneakyThrows;
 
 import java.util.HashMap;
 
 public class CharacterFactory {
 
-    public HashMap<Integer, Class<? extends Character>> hm = new HashMap<Integer, Class<? extends Character>>();
-
+    private HashMap<Integer, Class<? extends Character>> hm = new HashMap<Integer, Class<? extends Character>>();
 
     public CharacterFactory() {
         hm.put(0, Hobbit.class);
@@ -19,11 +19,11 @@ public class CharacterFactory {
 
     }
 
+    @SneakyThrows
     public Character createCharacter() throws IllegalAccessException, InstantiationException {
+        return hm.get(Config.generateRandom(0, hm.size() - 1)).getDeclaredConstructor().newInstance();
+//        return hm.get(Config.generateRandom(0, hm.size())).getDeclaredConstructor().newInstance();
 //        return hm.get(Config.generateRandom(0, hm.size() - 1)).getConstructor().newInstance();
-        return hm.get(Config.generateRandom(0, hm.size() - 1)).newInstance();
-//        hm.get(Config.generateRandom(0, hm.size())).getDeclaredConstructor().newInstance();
-
+//        return hm.get(Config.generateRandom(0, hm.size() - 1)).newInstance();
     }
-
 }
